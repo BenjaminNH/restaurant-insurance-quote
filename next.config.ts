fdf22c6a92d465a60a7cffc63f7088fdb306dd46
BenjaminNH <56266135+BenjaminNH@users.noreply.github.com>
2026-09-05 20:23:43 +0800

    fix: allow LAN origin during development

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  allowedDevOrigins: ["127.0.0.1"],
+  allowedDevOrigins: ["127.0.0.1", "192.168.31.102"],
 };
 
 export default nextConfig;
