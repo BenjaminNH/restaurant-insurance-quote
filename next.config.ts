c20845fe568f2a7f09c540477a9047287475ce4d
BenjaminNH <56266135+BenjaminNH@users.noreply.github.com>
2026-09-06 12:06:31 +0800

    fix: disable draggable Next dev indicator

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   allowedDevOrigins: ["127.0.0.1", "192.168.31.102"],
+  devIndicators: false,
 };
 
 export default nextConfig;
